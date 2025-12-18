631d72a7756a60828b9fe7bd9d655eafc37d3964
TERE NAAM <eukgjds@gmail.com>
2025-12-18 18:45:45 +0545

    adding component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const App = () => {
+
+function App() {
+ 
   return (
-    <div className="main">
-      <h1>where are they</h1>
+    <div className="w-full h-screen bg-zinc-800 p-5">
+      <div className="w-80 h-42 bg-red-800 rounded-3xl p-5 text-white text-3xl ">Hello kgarira
+      </div>
+     <products/>
     </div>
   );
 }
-
+ 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM, { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
 
 
