7ba4b90aaa6bad36de7b163737501f2ca2a06159
Samir-Sunar18 <worldorderno@gmail.com>
2025-12-18 18:59:25 +0545

    test contribution

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import React from 'react'
 function App() {
  
   return (
+    <div>
     <div className="w-full h-screen bg-zinc-800 p-5">
       <div className="w-80 h-42 bg-red-800 rounded-3xl p-5 text-white text-3xl ">Hello kgarira
       </div>
-     <products/>
+      
+    </div>
     </div>
   );
 }
