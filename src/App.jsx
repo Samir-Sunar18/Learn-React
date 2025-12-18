0c55ad4777899a113b4cf4202b89a73d9f8a46d8
Samir-Sunar18 <worldorderno@gmail.com>
2025-12-18 23:23:47 +0545

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
+import Products from "./Products";
 
 function App() {
-  const [a, SetA] = useState(24);
+  const [a, setA] = useState(false); // Conditional rendering
 
   return (
-    <div className="w-full h-screen bg-zinc-900 text-white p-6">
-      <h1 className="text-4xl">Value: {a}</h1>
-      <button
-        onClick={() => SetA(a + 1)} // inline function
-        className="mt-4 px-6 py-3 bg-purple-500 rounded-full text-white font-bold hover:bg-purple-600  hover:scale-110 transition-transform duration-300 cursor-pointer shadow-lg"
-      >
-        Increase
-      </button>
+    <div className="w-full h-screen text-white bg-gray-900 flex items-center justify-center">
+      <Products
+        age={34}
+        data={{ name: "Samir", address: "Lalitpur", Phone: "239873827" }}
+      />
     </div>
   );
 }
--- src/products.jsx
+++ src/products.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Products() {
-  return(
-   <div className=" w-full h-90 rounded-3xl border-red-50 bg-zinc-700">
+function Products({ age, data }) {
+  const [a, setA] = useState(false);
 
-   </div>
-   ) 
+  return (
+    <div className="text-white w-full h-60 p-6 bg-zinc-900">
+      {/* Display data from props */}
+      <h1 className="text-2xl mb-2">{data.name}</h1>
+      <p>Age: {age}</p>
+      <p>Address: {data.address}</p>
+      <p>Phone: {data.Phone}</p>
+      <h4 className={`${a === false ? "text-red-500" : "text-blue-400"}`}>
+        {a === false ? "hello" : "hey"}
+      </h4>
+      <button
+        onClick={() => setA(!a)}
+        className="mt-4 px-6 py-2 bg-purple-500 rounded hover:bg-purple-600"
+      >
+        Change
+      </button>
+    </div>
+  );
 }
 
 export default Products;
+
+// {To be Contd..}
+
