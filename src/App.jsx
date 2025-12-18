8f5f0b6668c7d1fa0bfac76d9da569176d98d43d
Samir-Sunar18 <worldorderno@gmail.com>
2025-12-18 20:41:09 +0545

    onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import React, { useState } from "react";
 
 function App() {
-  const [a, b] = useState(24);
-  {
-    const increaseValue = () => {
-      setA(a + 1); // adds 1 to current value
-    };
-    return (
-      <div className="w-full h-screen bg-zinc-900 text-white p-6">
-        <h1 className="text-4xl">Value: {a}</h1>
-        <button
-          onClick={() => b(26)}
-          className="mt-4 px-4 py-2 bg-blue-500 rounded hover:bg-blue-600"
-        >
-          Increase
-        </button>
-      </div>
-    );
-  }
+  const [a, SetA] = useState(24);
+
+  return (
+    <div className="w-full h-screen bg-zinc-900 text-white p-6">
+      <h1 className="text-4xl">Value: {a}</h1>
+      <button
+        onClick={() => SetA(a + 1)} // inline function
+        className="mt-4 px-6 py-3 bg-purple-500 rounded-full text-white font-bold hover:bg-purple-600  hover:scale-110 transition-transform duration-300 cursor-pointer shadow-lg"
+      >
+        Increase
+      </button>
+    </div>
+  );
 }
 
 export default App;
