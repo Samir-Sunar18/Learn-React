9626326e2c373fc3e3c0e962dbdf8e4f3174543f
Samir-Sunar18 <worldorderno@gmail.com>
2025-12-18 19:01:08 +0545

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="w-full h-screen bg-zinc-800 p-5">
       <div className="w-80 h-42 bg-red-800 rounded-3xl p-5 text-white text-3xl ">Hello kgarira
       </div>
-      
+      <Products/>
     </div>
     </div>
   );
